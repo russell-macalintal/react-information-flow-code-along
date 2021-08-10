6f726884ed466f419b984241183639ec8febf5ed
russell-macalintal <russell-macalintal@gmail.com>
2021-08-10 04:22:12 -0400

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,25 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: "#FFF"
     }
+  }
+
+  // Defining changeColor function using an arrow function is critical in making sure that 'this' variable refers to the Parent component instance when the instance method is invoked within the Child component class
+  // If changeColor is defined as a regular function declaration, 'this' will not be binded to the Parent component instance's execution context and the function will attempt to 'work' on the Child component instance when it is invoked (causing errors)
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor.bind(this)}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor.bind(this)}/>
       </div>
     )
   }
